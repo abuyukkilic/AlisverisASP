74004e53348bb42663b88708eccbbf717a666082
Aykut Büyükkılıç <aykut.buyukkilic@gmail.com>
2018-10-26 14:29:35 +0300

    SON HALİ

---
+++ Abuyukkilic.Mvc.WebUI/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Abuyukkilic.Mvc.WebUI.Entity;
+using Abuyukkilic.Mvc.WebUI.Models;
+
+namespace Abuyukkilic.Mvc.WebUI.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class OrderController : Controller
+    {
+        DataContext db = new DataContext();
+
+        // GET: Order
+        public ActionResult Index()
+        {
+            var orders = db.Order.Select(i => new AdminOrderModel()
+            {
+                Id = i.Id,
+                Total = i.Total,
+                OrderNumber = i.OrderNumber,
+                OrderDate = i.OrderDate,
+                OrderState = i.OrderState,
+                Count = i.Orderlines.Count
+            }).OrderByDescending(i => i.OrderDate).ToList();
+            return View(orders);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View()
+        }
+    }
+    
+}
